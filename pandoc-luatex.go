d97a62810c0d069d147e4ef8118b7780f9cc3952
bhuvy <bhuvan.venkatesh21@gmail.com>
2019-05-07 16:27:45 -0500

    Adding base

---
+++ pandoc-luatex.go
@@ -0,0 +1,52 @@
+package main
+
+import (
+    "flag"
+    "fmt"
+    "os"
+
+    "github.com/Azure/golua/lua"
+    "github.com/Azure/golua/std"
+)
+
+var (
+    trace bool = false
+    debug bool = false
+    tests bool = false
+)
+
+func must(err error) {
+    if err != nil {
+        fmt.Fprintf(os.Stderr, "fatal: %v\n", err)
+        os.Exit(1)
+    }
+}
+
+func init() {
+    flag.BoolVar(&debug, "debug", debug, "enable verbose logging")
+    flag.BoolVar(&trace, "trace", trace, "enable tracing")
+    flag.BoolVar(&tests, "tests", trace, "execute tests")
+    flag.Parse()
+}
+
+func main() {
+    /*if flag.NArg() < 1 {
+        must(fmt.Errorf("missing arguments"))
+    }*/
+
+    var opts = []lua.Option{lua.WithTrace(trace), lua.WithVerbose(debug)}
+    state := lua.NewState(opts...)
+    defer state.Close()
+    std.Open(state)
+
+    startup := `
+os.execute = function(...) end
+tex = {}
+tex.write = print
+print = function(...) end
+`
+
+    state.ExecText(startup)
+    state.ExecText("tex.write(\"Hello\")")
+    state.ExecText("print(\"Hello\")")
+}
